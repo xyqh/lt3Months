12d959acfd16c5501aa107050ee53374323d2b09
haixin <haixin1225@outlook.com>
2023-11-24 00:36:23 +0800

    anotherMinDistance

--- whoNamedYaozi.cpp
+++ whoNamedYaozi.cpp
@@ -92,8 +92,118 @@ int minDistance(vector<int>& s1, vector<int>& s2, vector<int>& s3){
     return minD;
 }
 
+#include <vector>
+#include <algorithm>
+#include <iostream>
+int anotherMinDistance(const vector<int>& s1, const vector<int>& s2, const vector<int>& s3)
+{
+    int calc_count = 0;
+    auto calc = [&calc_count, s1, s2, s3](int i, int j, int k) {
+        ++calc_count;
+        return std::abs(s1[i] - s2[j]) + std::abs(s2[j] - s3[k]) + std::abs(s3[k] - s1[i]);
+    };
+
+    // 获取a b c的中位数
+    auto _mid_of_three = [](int a, int b, int c) {
+        std::vector<int> tmp = { a, b, c };
+        std::sort(tmp.begin(), tmp.end());
+        return tmp[1];
+    };
+
+    auto _get_next_baseline = [s1, s2, s3](int i, int j, int k) {
+        auto _get_max = [](int a, int b, int c) {
+            return std::max(a, std::max(b, c));
+        };
+
+        std::vector<int> tmp;
+        tmp.push_back(_get_max(s1[i], s2[j], s3[k]));
+        if (i + 1 < s1.size()) {
+            tmp.push_back(s1[i + 1]);
+        }
+        if (j + 1 < s2.size()) {
+            tmp.push_back(s2[j + 1]);
+        }
+        if (k + 1 < s3.size()) {
+            tmp.push_back(s3[k + 1]);
+        }
+
+        std::sort(tmp.begin(), tmp.end());
+        return tmp[1];
+    };
+
+    int min_D = INT32_MAX;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    int baseline = _mid_of_three(s1[0], s2[0], s3[0]);
+    int baseline_end = _mid_of_three(s1.back(), s2.back(), s3.back());
+
+    int len1 = abs(s1[0] - baseline);
+    int len2 = abs(s2[0] - baseline);
+    int len3 = abs(s3[0] - baseline);
+
+    while (baseline < baseline_end) {
+
+        int calc_D = calc(i, j, k);
+
+        std::cout << "baseline:" << baseline << std::endl;
+        std::cout << "D:" << min_D << " trituple:(" << s1[i] << "," << s2[j] << "," << s3[k] << ") " << std::endl;
+
+        if (calc_D < min_D) {
+            min_D = calc_D;
+            if (min_D == 0) {
+                break;
+            }
+        }
+
+        len1 = abs(s1[i] - baseline);
+        len2 = abs(s2[j] - baseline);
+        len3 = abs(s3[k] - baseline);
+
+        // 将i, j, k 都逼近baseline
+        while (i + 1 < s1.size() && abs(s1[i + 1] - baseline) <= len1) {
+            len1 = abs(s1[i + 1] - baseline);
+            i = i + 1;
+            std::cout << "i++:" << std::endl;
+        }
+        while (j + 1 < s2.size() && abs(s2[j + 1] - baseline) <= len2) {
+            len2 = abs(s2[j + 1] - baseline);
+            j = j + 1;
+            std::cout << "j++:" << std::endl;
+        }
+        while (k + 1 < s3.size() && abs(s3[k + 1] - baseline) <= len3) {
+            len3 = abs(s3[k + 1] - baseline);
+            k = k + 1;
+            std::cout << "k++:" << std::endl;
+        }
+
+        // 将 baseline 往右移动
+        int mid_value = _mid_of_three(s1[i], s2[j], s3[k]);
+        if (mid_value == baseline) {
+            // 如果当前三点abc中的中位数就是baseline， 那么baseline选取 S1，S2，S3 比 baseline大的下一个数
+            baseline = _get_next_baseline(i, j, k);
+        } else {
+            baseline = mid_value;
+        }
+    }
+
+    int newcalc_D = calc(i, j, k);
+
+    if (newcalc_D < min_D) {
+        min_D = newcalc_D;
+    }
+
+    std::cout << "calc_count:" << calc_count << std::endl;
+    std::cout << "D:" << min_D << " trituple:(" << s1[i] << "," << s2[j] << "," << s3[k] << ") " << std::endl;
+
+    return min_D;
+}
+
+
 int main(){
     vector<int> s1 = {-1,0,9}, s2 = {-25,-10,10,11}, s3 = {2,9,17,30,41};
+    int anotherMinD = anotherMinDistance(s1, s2, s3);
     int minD = minDistance(s1, s2, s3);
     cout << minD << endl;
     return 0;

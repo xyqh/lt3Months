ac0ed5909c891a2fb5cb19720fba302e0058364a
千行 <11842314+dedala@user.noreply.gitee.com>
2023-11-27 00:39:45 +0800

    tab modify

--- 828.cpp
+++ 828.cpp
@@ -1,19 +1,19 @@
-    #include "LeetCodeBase.h"
+#include "LeetCodeBase.h"
 
-    int uniqueLetterString(string s) {
-        int n = s.length(), unique = 0;
-        unordered_map<char, vector<int>> index;
-        for(int i = 0; i < n; ++i){
-            index[s[i]].emplace_back(i);
-        }
-
-        for(auto&&[_, arr]:index){
-            arr.insert(arr.begin(), -1);
-            arr.emplace_back(n);
-            for(int i = 1; i < arr.size() - 1; ++i){
-                unique += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i]);
-            }
-        }
-
-        return unique;
+int uniqueLetterString(string s) {
+    int n = s.length(), unique = 0;
+    unordered_map<char, vector<int>> index;
+    for(int i = 0; i < n; ++i){
+        index[s[i]].emplace_back(i);
     }
+
+    for(auto&&[_, arr]:index){
+        arr.insert(arr.begin(), -1);
+        arr.emplace_back(n);
+        for(int i = 1; i < arr.size() - 1; ++i){
+            unique += (arr[i] - arr[i - 1]) * (arr[i + 1] - arr[i]);
+        }
+    }
+
+    return unique;
+}

0e63b289981a98c4719c14045d33e7eac883a7c1
xyqh <563784623@qq.com>
2023-11-29 13:49:13 +0800

    serialize&deserialize listnode&treenode

--- LeetCodeBase.h
+++ LeetCodeBase.h
@@ -54,3 +54,79 @@ struct TreeNode
         right(_right)
         {};
 };
+
+vector<int> serializeListNode(ListNode *node){
+    vector<int> serialize;
+    while(node){
+        serialize.push_back(node->val);
+        node = node->next;
+    }
+    return serialize;
+}
+
+ListNode* DeSerializeListNode(const vector<int>& lists){
+    int n = lists.size();
+    if(n == 0){
+        return nullptr;
+    }
+    ListNode *dummyNode = new ListNode(0), *node = dummyNode;
+    for(const int &val:lists){
+        node->next = new ListNode(val);
+        node = node->next;
+    }
+    return dummyNode->next;
+}
+
+vector<string> serializeTreeNode(TreeNode *root){
+    queue<TreeNode*> que;
+    vector<string> serialize;
+    que.push(root);
+    bool nextAllNullptr = false;
+    while(!nextAllNullptr && !que.empty()){
+        nextAllNullptr = true;
+        int n = que.size();
+        TreeNode *node = nullptr;
+        for(int i = 0; i < n; ++i){
+            node = que.front();
+            que.pop();
+            serialize.push_back(node ? to_string(node->val) : "null");
+            que.push(node ? node->left : nullptr);
+            que.push(node ? node->right : nullptr);
+            if(node && (node->left || node->right)){
+                nextAllNullptr = false;
+            }
+        }
+    }
+
+    return serialize;
+}
+
+TreeNode *createTreeNode(const string& str){
+    if(str == "null") return nullptr;
+    return new TreeNode(atoi(str.c_str()));
+}
+
+TreeNode *DeSerializeTreeNode(const vector<string>& nodes){
+    int n = nodes.size(), i = 0;
+    if(n < 1) return nullptr;
+    string val = nodes[i++];
+    TreeNode *node = createTreeNode(val);
+    if(node == nullptr){
+        return nullptr;
+    }
+    TreeNode *root = node;
+    queue<TreeNode*> que;
+    que.push(node);
+    while(i < n){
+        node = que.front();
+        que.pop();
+        TreeNode *left = nullptr, *right = nullptr;
+        if(node){
+            node->left = left = createTreeNode(nodes[i++]);
+            node->right = right = createTreeNode(nodes[i++]);
+        }
+        que.push(left);
+        que.push(right);
+    }
+    return root;
+}

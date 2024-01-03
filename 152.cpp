a6ee43acbf1a356178db5f2c135bfa3b18b0c043
xyqh <563784623@qq.com>
2024-01-03 20:11:47 +0800

    01/03_1

---
+++ 152.cpp
@@ -0,0 +1,5 @@
+    #include "LeetCodeBase.h"
+
+    int maxProduct(vector<int>& nums){
+        
+    }
---
+++ 300.cpp
@@ -0,0 +1,16 @@
+    #include "LeetCodeBase.h"
+
+    int lengthOfLIS(vector<int>& nums){
+        int n = nums.size(), ans = 0;
+        vector<int> dp(n, 1);
+        for(int i = 0; i < n; ++i){
+            for(int j = i - 1; j >= 0; --j){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+
+        return ans;
+    }

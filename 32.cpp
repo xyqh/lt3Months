e4108e9c4bcdf2947f846c5cc799b397d0312189
xyqh <563784623@qq.com>
2024-01-29 11:30:33 +0800

    01/29_1

--- 32.cpp
+++ 32.cpp
@@ -1,5 +1,25 @@
     #include "LeetCodeBase.h"
 
     int longestValidParentheses(string s){
-        
+        int n = s.length(), ans = 0;
+        vector<int> left(n, 0), right(n, 0);
+        for(int i = 0; i < n; ++i){
+            if(i > 0){
+                left[i] = left[i - 1];
+                right[i] = right[i - 1];
+            }
+
+            if(s[i] == '('){
+                right[i] = max(min(right[i], left[i] - 1), 0);
+                ++left[i];
+            }else{
+                ++right[i];
+                if(right[i] > left[i]){
+                    left[i] = 0;
+                }
+            }
+            ans = max(ans, min(left[i], right[i]) * 2);
+        }
+
+        return ans;
     }
